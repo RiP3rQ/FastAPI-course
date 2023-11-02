807c7929df98e9ecdaecc96caf31ca214642c229
RiP3rQ <rafalpompa2000@gmail.com>
2023-11-02 20:58:17 +0100

    basic api

---
+++ main.py
@@ -0,0 +1,44 @@
+from typing import  Optional
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from random import randint
+
+app = FastAPI()
+
+# title str, content str
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [
+    {"id": 1,"title": "Hello", "content": "World"},
+    {"id": 2,"title": "Fast", "content": "API"},
+    {"id": 3,"title": "Python", "content": "Programming"},
+]
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"Data": my_posts}
+
+@app.post("/posts")
+def create_post(newPost: Post):
+    print (newPost)
+    post_dict = newPost.model_dump()
+    post_dict["id"] = randint(0,1000000)
+    my_posts.append(post_dict)
+    return post_dict
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+    
+    raise HTTPException(status_code=404, detail="Post not found")
+
