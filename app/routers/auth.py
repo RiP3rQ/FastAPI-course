ccdc06864b6e693a078629d0f228fb00b6708141
RiP3rQ <rafalpompa2000@gmail.com>
2023-11-03 18:22:53 +0100

    creating JWT and returning to user

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,8 +1,9 @@
 # ------------------------------------------------------------------ IMPORTS ------------------------------------------------------------------ #
 
 from fastapi import HTTPException, status, Response, Depends, APIRouter
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from .. import models, schemas, utils
+from .. import models, schemas, utils, oauth2
 from ..database import get_db
 
 router = APIRouter(
@@ -11,8 +12,8 @@ router = APIRouter(
 
 # ------------------------------------------------------------------ AUTH ------------------------------------------------------------------ #
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     # check if user exists
     if user == None:
@@ -23,7 +24,7 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect credentials")
 
     # create JWT token
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
 
     # return token
-
-    return {"token": "token"}
+    return {"access_token" : access_token, "token_type" : "bearer"}
